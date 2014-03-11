59c28513cd98312c15d45c0ff047570c1229f926
smark <smark12007@yahoo.com.tw>
2014-03-11 21:30:15 +0800

    Update myrobot.js

--- robot/myrobot.js
+++ robot/myrobot.js
@@ -5,7 +5,8 @@
   MyRobot = (function(superclass){
     var prototype = extend$((import$(MyRobot, superclass).displayName = 'MyRobot', MyRobot), superclass).prototype, constructor = MyRobot;
     prototype.onIdle = function(){
-      this.move_forwards(200);
+      this.shoot();
+      this.move_forwards(50);
       this.turn_turret_left(10);
       this.turn_right(90);
     };
